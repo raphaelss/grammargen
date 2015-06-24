330cc46dc3348c3e8961bba308603d8cb172243c
Raphael Santos <contact@raphaelss.com>
2015-06-24 10:52:10 -0300

    add license and fix style issues

--- grammar.cpp
+++ grammar.cpp
@@ -1,40 +1,40 @@
 #include "grammar.hpp"
 
 Grammar::Grammar(const std::string& axiom_)
-	:axiom(axiom_), rules(), rndGen(std::random_device()()) {}
+  :axiom(axiom_), rules(), rndGen(std::random_device()()) {}
 
 void Grammar::addRule(int lhs, std::string str, double prob)
 {
-	rules[lhs].addClause(str, prob);
+  rules[lhs].addClause(str, prob);
 }
 
 bool Grammar::hasRule(int ch) const
 {
-	return rules.count(ch);
+  return rules.count(ch);
 }
 
 const std::string& Grammar::operator()(int ch)
 {
-	return rules.at(ch).choose(distr, rndGen);
+  return rules.at(ch).choose(distr, rndGen);
 }
 
 void Grammar::Rule::addClause(const std::string& str, double prob)
 {
-	clauses.emplace_back(str);
-	probs.emplace_back(prob);
+  clauses.emplace_back(str);
+  probs.emplace_back(prob);
 }
 
 const std::string& Grammar::Rule::choose(
-	std::discrete_distribution<unsigned>& distr,
-	std::mt19937_64& rndGen)
+  std::discrete_distribution<unsigned>& distr,
+  std::mt19937_64& rndGen)
 {
-	if(clauses.size() == 1) {
-		return clauses[0];
-	}
-	distr.param(std::discrete_distribution<unsigned>::param_type(
-		probs.cbegin(), probs.cend())
-	);
-	return clauses[distr(rndGen)];
+  if(clauses.size() == 1) {
+    return clauses[0];
+  }
+  distr.param(std::discrete_distribution<unsigned>::param_type(
+    probs.cbegin(), probs.cend())
+  );
+  return clauses[distr(rndGen)];
 }
 
 
--- grammar.hpp
+++ grammar.hpp
@@ -8,26 +8,26 @@
 class Grammar
 {
 public:
-	Grammar(const std::string& axiom_);
-	void addRule(int lhs, std::string str, double prob=1);
-	const std::string& operator()(int ch);
-	bool hasRule(int ch) const;
+  Grammar(const std::string& axiom_);
+  void addRule(int lhs, std::string str, double prob=1);
+  const std::string& operator()(int ch);
+  bool hasRule(int ch) const;
 
-	std::string axiom;
+  std::string axiom;
 private:
-	struct Rule
-	{
-		void addClause(const std::string& str, double prob=1);
-		const std::string& choose(
-			std::discrete_distribution<unsigned>& distr,
-			std::mt19937_64& rndGen);
-		std::vector<std::string> clauses;
-		std::vector<double> probs;
-	};
+  struct Rule
+  {
+    void addClause(const std::string& str, double prob=1);
+    const std::string& choose(
+      std::discrete_distribution<unsigned>& distr,
+      std::mt19937_64& rndGen);
+    std::vector<std::string> clauses;
+    std::vector<double> probs;
+  };
 
-	std::unordered_map<int, Rule> rules;
-	std::mt19937_64 rndGen;
-	std::discrete_distribution<unsigned> distr;
+  std::unordered_map<int, Rule> rules;
+  std::mt19937_64 rndGen;
+  std::discrete_distribution<unsigned> distr;
 };
 
 #endif // GRAMMAR_H
